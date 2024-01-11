ee311bea29476cca261da62bd5b0417052b632a6
Swazimodo <swn@duck.com>
2024-01-11 10:16:45 -0700

    fix: unit test warnings from state updates after render

--- src/__tests__/unit/App.test.tsx
+++ src/__tests__/unit/App.test.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 
 import { App } from 'App';
 
-test('renderApp_shouldHaveLayoutElements', () => {
-  const { container } = render(<App />);
+// prevent the app from loading data after the initial render
+// this will allow testing the loading state
+jest.mock('common', () => {
+  const originalModule = jest.requireActual('common');
 
-  const headerElement = container.querySelector('header');
-  const navElement = container.querySelector('nav');
-  const mainElement = container.querySelector('main');
-  const footerElement = container.querySelector('footer');
+  return {
+    __esModule: true,
+    ...originalModule,
+    willSucceed: () => new Promise((resolve, reject) => { })
+  };
+});
+
+test('renderApp_shouldHaveLayoutElements', async () => {
+  const { container, findByRole } = render(<App />);
+
+  const headerElement = await findByRole("banner")
+  const navElement = await findByRole("navigation")
+  const mainElement = await findByRole("main")
+  const footerElement = await findByRole("contentinfo")
+
   expect(headerElement).toBeInTheDocument();
   expect(navElement).toBeInTheDocument();
   expect(mainElement).toBeInTheDocument();
