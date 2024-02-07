5d592311658d02a5da1b3102c5b8b7e272fa6829
Swazimodo <swn@duck.com>
2024-02-06 21:55:04 -0700

    fix: simplify promise result

--- src/common/mockApiCalls.ts
+++ src/common/mockApiCalls.ts
@@ -12,14 +12,10 @@ export const randomSuccessOrFailure = (value?: any) => {
 
 export const willSucceed = (value?: any) => {
   console.log(`mock api call with: ${value}`)
-  return new Promise((resolve, reject) => {
-    resolve({ responseCode: 200, message: "success" })
-  })
+  return Promise.resolve({ responseCode: 200, message: "success" })
 }
 
 export const willFail = (value?: any) => {
-  console.log(`mock api call with: ${value}`)
-  return new Promise((resolve, reject) => {
-    reject({ responseCode: 404, message: "not found" })
-  })
+  console.log(`mock failing api call with: ${value}`)
+  return Promise.reject({ responseCode: 404, message: "not found" })
 }
