6d73e6227c30833ba097c7b0514adf046a239784
Swazimodo <swn@duck.com>
2024-02-06 22:43:46 -0700

    feat: add nav buttons

--- src/layout/nav.tsx
+++ src/layout/nav.tsx
@@ -1,10 +1,32 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 
 export const Nav: FC = () => {
-  return <SiteNav>nav</SiteNav>
+  return <SiteNav>
+    <ul>
+      <li><Link to="table">Resource Table</Link></li>
+      <li><Link to="TODO">TODO</Link></li>
+    </ul>
+  </SiteNav>
 }
 
 const SiteNav = styled.nav`
   background-color: #4f1492;
+
+  ul {
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    padding: 0 8px;
+  }
+
+  a:link,
+  a:visited,
+  a:hover,
+  a:active {
+    color: orange;
+  }
 `
